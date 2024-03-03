a0e6685e7bd10c5e36755be2966bf3854315c58d
juvenal <njuvenal62@gmail.com>
2024-03-03 22:47:28 +0100

    premiere modification du fichier ppm.h

--- ppm.h
+++ ppm.h
@@ -1,10 +1,10 @@
 #ifndef PPM_H
 #define PPM_H
 
-struct ppm_pixel {
-  unsigned char r;
-  unsigned char g;
-  unsigned char b;
+struct ppm_pixel {   // cette Structure représentant un pixel avec des valeurs RGB.
+  unsigned char r;  /**< Composante rouge */
+  unsigned char g;  /**< Composante verte */
+  unsigned char b;  /**< Composante bleuer */
 };
 
 static inline void ppm_setpixel(struct ppm_pixel *px, unsigned char r,
@@ -13,12 +13,25 @@ static inline void ppm_setpixel(struct ppm_pixel *px, unsigned char r,
   px->g = g;
   px->b = b;
 }
+/**
+ * static inline void Définit les valeurs RGB d'un pixel et prends en parametres 
+ *px Pointeur vers la structure du pixel.
+ * r Valeur de la composante rouge.
+ * g Valeur de la composante verte.
+ * b Valeur de la composante bleue.
+ */
 
+
+//Structure représentant une image PPM.
 struct ppm_image {
-  unsigned int width;
-  unsigned int height;
-  struct ppm_pixel *px;
+  unsigned int width; //Largeur de l'image 
+  unsigned int height; // Hauteur de l'image 
+  struct ppm_pixel *px; //Hauteur de l'image 
 };
+
+
+
+
 
 int ppm_image_init(struct ppm_image *im, int w, int h);
 int ppm_image_release(struct ppm_image *im);
