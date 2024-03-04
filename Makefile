28b2c950b8ed92e55b983bb5495b0bb925d2ce2b
MNJuvenal <njuvenal62@gmail.com>
2024-03-04 01:58:28 +0100

    modification du makefile et ajout des executable

--- Makefile
+++ Makefile
@@ -1,19 +1,37 @@
-CC=gcc
-CFLAGS=-O3 -g
- 
-TARGET=test mandel
- 
-all: $(TARGET)
- 
-libppm.so : ppm.c
-	$(CC) $(CFLAGS)  -fpic -shared $^ -o $@
- 
-test: main.c libppm.so
-	$(CC) $(CFLAGS) $(LDFLAGS) main.c  -lppm -L. -o $@
+CC = gcc
+CFLAGS = -Wall -Wextra -std=c99
+LDFLAGS = -lm
 
-mandel: mandel.c libppm.so
-	$(CC) $(CFLAGS) $(LDFLAGS) $< -lppm -L.  -o $@
- 
+# Liste des fichiers source
+SRC_FILES = mandel.c main.c ppm.c
+# Liste des fichiers objets correspondants
+#OBJ_FILES = $(SRC_FILES:.c=.o)
+# Noms des exécutables
+EXEC_TEST = test
+EXEC_MANDEL = mandel
+# Nom de la bibliothèque partagée
+LIB_NAME = libppm.so
 
+# Règle par défaut
+all: $(EXEC_TEST) $(EXEC_MANDEL) $(LIB_NAME)
+
+# Règle pour créer l'exécutable 'test'
+$(EXEC_TEST): main.o ppm.o
+	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS)
+
+# Règle pour créer l'exécutable 'mandel'
+$(EXEC_MANDEL): mandel.o ppm.o
+	$(CC) $(CFLAGS) $^ -o $@ $(LDFLAGS)
+
+# Règle pour créer la bibliothèque partagée 'libppm.so'
+$(LIB_NAME): ppm.o
+	$(CC) -shared -o $@ $^
+
+# Règle générale pour la création des fichiers objets
+%.o: %.c
+	$(CC) $(CFLAGS) -fPIC -c $< -o $@
+
+# Nettoyer les fichiers temporaires et les exécutables
 clean:
-	rm -fr $(TARGET) *.so
+	rm -f $(OBJ_FILES) $(EXEC_TEST) $(EXEC_MANDEL) $(LIB_NAME)
+
--- ppm.h
+++ ppm.h
@@ -19,22 +19,40 @@ static inline void ppm_setpixel(struct ppm_pixel *px, unsigned char r,
  * r Valeur de la composante rouge.
  * g Valeur de la composante verte.
  * b Valeur de la composante bleue.
- */
+ **/
 
 
 //Structure représentant une image PPM.
 struct ppm_image {
   unsigned int width; //Largeur de l'image 
   unsigned int height; // Hauteur de l'image 
-  struct ppm_pixel *px; //Hauteur de l'image 
+  struct ppm_pixel *px; // Tableau de pixels
 };
 
-
-
-
-
 int ppm_image_init(struct ppm_image *im, int w, int h);
+
+/**
+ * cette fonction  Initialise une image PPM avec la largeur et la hauteur spécifiées.
+ * et avec parametres :
+          * Pointeur vers la structure de l'image à initialiser.
+        * w Largeur de l'image.
+        * h Hauteur de l'image.
+  * et retourne :
+         0 en cas de succès, -1 en cas d'échec.
+ **/
+
 int ppm_image_release(struct ppm_image *im);
+
+/**
+ * cette fonction  Définit les valeurs RGB d'un pixel aux coordonnées spécifiées dans l'image.
+ * 
+ *  im Pointeur vers la structure de l'image.
+ *  x Coordonnée X du pixel.
+ *  y Coordonnée Y du pixel.
+ *  r Valeur de la composante rouge.
+ *  g Valeur de la composante verte.
+ *  b Valeur de la composante bleue.
+ */
 
 static inline void ppm_image_setpixel(struct ppm_image *im, int x, int y,
                                       unsigned char r, unsigned char g,
@@ -42,7 +60,21 @@ static inline void ppm_image_setpixel(struct ppm_image *im, int x, int y,
   struct ppm_pixel *px = im->px + im->width * y + x;
   ppm_setpixel(px, r, g, b);
 }
+/**
+ * cette fonction  Écrit les données de l'image PPM dans un fichier.
+ * 
+ * im Pointeur vers la structure de l'image.
+ * Chemin vers le fichier de sortie.
+ * et return 0 en cas de succès, -1 en cas d'échec.
+ */
 
 int ppm_image_dump(struct ppm_image *im, char *path);
+/**
+ * cette fonction  Écrit les données de l'image PPM dans un fichier.
+ * 
+ *  im Pointeur vers la structure de l'image.
+ *  path Chemin vers le fichier de sortie.
+ * retourne  0 en cas de succès, -1 en cas d'échec.
+ */
 
-#endif /* PPM_H */
+#endif 
